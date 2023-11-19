a877ebb7e5ad2a594b512c5fc12f0a15cafb8b27
bach29072004 <bach29072004@gmail.com>
2023-11-19 23:22:18 +0700

    bach update

--- src/Controller/Update.java
+++ src/Controller/Update.java
@@ -1,4 +1,40 @@
 package Controller;
 
-public class Update {
+import Data.Data;
+import Data.Word;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Update implements Initializable {
+    @FXML
+    ListView<Word> wordList;
+    @FXML
+    TextField textField;
+    @FXML
+    TextArea t;
+    @FXML
+    TextField UpdateMean;
+    public void initialize(URL location, ResourceBundle resources) {
+        wordList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        wordList.getItems().clear();
+        wordList.getItems().addAll(Data.prefixSearch(""));
+    }
+    public void wordListClicked(){
+        Word word = wordList.getSelectionModel().getSelectedItem();
+        t.setText(word.getMean());
+    }
+    public void inputWordEventHandle(KeyEvent e) {
+        wordList.getItems().clear();
+        wordList.getItems().addAll(Data.prefixSearch(textField.getText()));
+    }
+    public void update(ActionEvent actionEvent) {
+
+    }
+
 }
