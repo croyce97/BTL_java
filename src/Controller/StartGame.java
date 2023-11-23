ca4ae998952bf214f5e3b746bd584df5a3b174a1
Nguyễn Quang Cảnh <125856531+croyce97@users.noreply.github.com>
2023-11-23 23:17:50 +0700

    Add files via upload

---
+++ src/Controller/StartGame.java
@@ -0,0 +1,12 @@
+package Controller;
+import Data.Data;
+import Data.Word;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+public class StartGame {
+
+
+}
