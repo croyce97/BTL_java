5b59f3f09ca17e21c248a5d6b81ad4e957b1084d
croyce97 <125856531+croyce97@users.noreply.github.com>
2023-11-26 16:31:27 +0700

    canh

--- src/Data/ConnectionToSQL.java
+++ src/Data/ConnectionToSQL.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class ConnectionToSQL {
     private Connection dataBaseLink;
     public Connection DataBaseConnection() {
-        String dataBasename = "dictionary";
+        String dataBasename = "classicmodels";
         String dataBaseUser = "root";
-        String passWord = "bachuet123";
-        String url = "jdbc:mysql://localhost:3307/" + dataBasename;
+        String passWord = "canh050104";
+        String url = "jdbc:mysql://localhost:3306/" + dataBasename;
         try {
 
             dataBaseLink = DriverManager.getConnection(url,dataBaseUser,passWord);
