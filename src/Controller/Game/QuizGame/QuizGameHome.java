a43b3679dce528279a732a429caff2441f472e09
bach29072004 <bach29072004@gmail.com>
2023-11-29 14:58:51 +0700

    bach update

--- src/Controller/Game/QuizGame/QuizGameHome.java
+++ src/Controller/Game/QuizGame/QuizGameHome.java
@@ -4,42 +4,35 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
 public class QuizGameHome {
-
     @FXML
-    private Button playquizbtn;
-
+    AnchorPane QuizAnchorPane;
     @FXML
-    private void initialize() {
+    AnchorPane anchorPane;
+    public void PlayNow(ActionEvent actionEvent) {
+        try {
+            QuizAnchorPane = FXMLLoader.load(getClass().getResource("/FXML/QuizGame.fxml"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-        playquizbtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-                try {
-                    Stage thisstage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-                    thisstage.close();
-
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("QuizGameResult.fxml"));
-                    Scene scene = new Scene(fxmlLoader.load());
-                    Stage stage = new Stage();
-                    stage.setScene(scene);
-                    stage.initStyle(StageStyle.TRANSPARENT);
-                    scene.setFill(Color.TRANSPARENT);
-                    stage.show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        setMainPane(QuizAnchorPane);
     }
+
+
+    public void setMainPane(AnchorPane pane) {
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(pane);
+    }
+
 
 }
