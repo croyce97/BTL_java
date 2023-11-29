334176bd958f634dcac46343deed2d37f965d39b
bach29072004 <bach29072004@gmail.com>
2023-11-29 13:52:55 +0700

    bach update

--- src/Controller/MatchingGame.java
+++ src/Controller/MatchingGame.java
@@ -12,31 +12,53 @@ public class MatchingGame {
     private final int boardSize = boardLength * boardLength;
     private final Random random = new Random();
 
-    private String[][] crosswordPairs ;
-    private ArrayList<String> Options;
+    private String[][] crosswordPairs;
+    private void loadCrosswordPairs() {
+        List<String[]> wordPairs = new ArrayList<>(Arrays.asList(
+                new String[]{"Water", "Nước"},
+                new String[]{"Chair", "Ghế"},
+                new String[]{"Tree", "Cây"},
+                new String[]{"Book", "Sách"},
+                new String[]{"Dog", "Chó"},
+                new String[]{"Movie", "Phim ảnh"},
+                new String[]{"Car", "Xe hơi"},
+                new String[]{"Music", "Nhạc"},
+                new String[]{"Apple", "Quả táo"},
+                new String[]{"Food", "Thức ăn"}
+        ));
 
+        // xáo trộn ngẫu nhiên
+        Collections.shuffle(wordPairs);
+        for (int i = 0; i < 10; i++) {
+            crosswordPairs[i] = wordPairs.get(i);
+        }
+    }
     public MatchingGame() {
         crosswordPairs = new String[4][2];
-        for (int i = 0; i < 4;i++) {
-                int k = random.nextInt(1,100);
+        try {
+            for (int i = 0 ;i  <4;i++) {
+                int k = random.nextInt(1, 1000);
                 String word = LoadWordFromDataBase.List.get(k).getWord();
-                int start = LoadWordFromDataBase.List.get(k).getMean().indexOf('\n');
-                int close = LoadWordFromDataBase.List.get(k).getMean().indexOf('\n', start +1);
-
-                String mean = LoadWordFromDataBase.List.get(k).getMean().substring(start+1,close);
+                int start = LoadWordFromDataBase.List.get(k).getMean().indexOf('-');
+                int close = LoadWordFromDataBase.List.get(k).getMean().indexOf('\n', start + 1);
+                if (close == -1) close = LoadWordFromDataBase.List.get(k).getMean().length() - 1;
+                String mean = LoadWordFromDataBase.List.get(k).getMean().substring(start + 1, close);
                 crosswordPairs[i][0] = word;
                 crosswordPairs[i][1] = mean;
-        }
+            }
 
-        Options = new ArrayList<>(Arrays.asList(
+
+         Options = new ArrayList<>(Arrays.asList(
                 crosswordPairs[0][1],crosswordPairs[0][0],
                 crosswordPairs[1][1],crosswordPairs[1][0],
                 crosswordPairs[2][1],crosswordPairs[2][0],
                 crosswordPairs[3][1],crosswordPairs[3][0]));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ngoai index");
+        }
     }
 
-
-
+    private ArrayList<String> Options;
     private final ArrayList<String> Board = new ArrayList<>(Arrays.asList(
             "","","","","",
             "","","","",""));
@@ -52,7 +74,6 @@ public class MatchingGame {
     }
 
     private void setupMatchingBoard() {
-       //= {{"Water", "Nước"}, {"Tree", "Cây"}, {"Movie", "Phim ảnh"}, {"Apple", "Quả táo"}}
         for (int i = 0; i < boardSize - 1; i++) {
             String MatchingOption = Options.get(i);
             int position = random.nextInt(boardSize - 1);
