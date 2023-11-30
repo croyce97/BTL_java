38dfff9f6ebbc9dd8b24e1ee827cd1ed7167712d
bach29072004 <bach29072004@gmail.com>
2023-12-01 00:57:16 +0700

    bach update

--- src/Controller/Game/QuizGame/QuizGameResult.java
+++ src/Controller/Game/QuizGame/QuizGameResult.java
@@ -1,49 +1,45 @@
 package Controller.Game.QuizGame;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.AnchorPane;
 
-public class QuizGameResult {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuizGameResult implements Initializable {
     @FXML
-    public Label remark, marks, markstext, correcttext, wrongtext;
+    public Label marks, markstext, correcttext, wrongtext;
+    @FXML
+    AnchorPane anchorPane;
+    @FXML
+    AnchorPane GameHome;
 
     @FXML
     public ProgressIndicator correct_progress, wrong_progress;
+    int point = QuizGameController.getCorrectInt();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        marks.setText(point + "/10");
+        markstext.setText(point + " Marks Scored");
+        correcttext.setText("Correct Answers :" + point);
+        wrongtext.setText( "Incorrect Answers : " + (10-point) );
+        correct_progress.setProgress((double) point /10);
+        wrong_progress.setProgress((double) (10-point) /10);
+        try {
+            GameHome = FXMLLoader.load(getClass().getResource("/FXML/GameHome.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-    int correct;
-    int wrong;
-
-//    @FXML
-//    private void initialize() {
-//        correct = QuizGameController.correct;
-//        wrong = QuizGameController.wrong;
-//
-//        correcttext.setText("Correct Answers : " + correct);
-//        wrongtext.setText("Incorrect Answers : " + String.valueOf(QuizGameController.wrong));
-//
-//        marks.setText(correct + "/10");
-//        float correctf = (float) correct/10;
-//        correct_progress.setProgress(correctf);
-//
-//        float wrongf = (float) wrong/10;
-//        wrong_progress.setProgress(wrongf);
-//
-//
-//        markstext.setText(correct + " Marks Scored");
-//
-//        if (correct<2) {
-//            remark.setText("Oh no..! You have failed the quiz. It seems that you need to improve your general knowledge. Practice daily! Check your results here.");
-//        } else if (correct>=2 && correct<5) {
-//            remark.setText("Oops..! You have scored less marks. It seems like you need to improve your general knowledge. Check your results here.");
-//        } else if (correct>=5 && correct<=7) {
-//            remark.setText("Good. A bit more improvement might help you to get better results. Practice is the key to success. Check your results here.");
-//        } else if (correct==8 || correct==9) {
-//            remark.setText("Congratulations! Its your hardwork and determination which helped you to score good marks. Check you results here.");
-//        } else if (correct==10) {
-//            remark.setText("Congratulations! You have passed the quiz with full marks because of your hardwork and dedication towards studies. Keep it up! Check your results here.");
-//        }
-
-
-    //}
+    }
+    public void PlayAgain(ActionEvent actionEvent) {
+        anchorPane.getChildren().clear();
+        anchorPane.getChildren().addAll(GameHome);
+    }
 }
