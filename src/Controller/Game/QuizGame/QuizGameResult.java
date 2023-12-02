84a93056093421c7ae2a45d24524854105d55cec
Nguyễn Quang Cảnh <125856531+croyce97@users.noreply.github.com>
2023-12-02 13:03:27 +0700

    add result text

--- src/Controller/Game/QuizGame/QuizGameResult.java
+++ src/Controller/Game/QuizGame/QuizGameResult.java
@@ -14,7 +14,7 @@ import java.util.ResourceBundle;
 
 public class QuizGameResult implements Initializable {
     @FXML
-    public Label marks, markstext, correcttext, wrongtext;
+    public Label marks, markstext, correcttext, wrongtext,remark;
     @FXML
     AnchorPane anchorPane;
     @FXML
@@ -31,6 +31,17 @@ public class QuizGameResult implements Initializable {
         wrongtext.setText( "Incorrect Answers : " + (10-point) );
         correct_progress.setProgress((double) point /10);
         wrong_progress.setProgress((double) (10-point) /10);
+        if (QuizGameController.getCorrectInt()<2) {
+            remark.setText("Oh no..! You have failed the quiz. It seems that you need to improve your general knowledge. Practice daily! Check your results here.");
+        } else if (QuizGameController.getCorrectInt()>=2 && QuizGameController.getCorrectInt()<5) {
+            remark.setText("Oops..! You have scored less marks. It seems like you need to improve your general knowledge. Check your results here.");
+        } else if (QuizGameController.getCorrectInt()>=5 && QuizGameController.getCorrectInt()<=7) {
+            remark.setText("Good. A bit more improvement might help you to get better results. Practice is the key to success. Check your results here.");
+        } else if (QuizGameController.getCorrectInt()==8 || QuizGameController.getCorrectInt()==9) {
+            remark.setText("Congratulations! Its your hardwork and determination which helped you to score good marks. Check you results here.");
+        } else if (QuizGameController.getCorrectInt()==10) {
+            remark.setText("Congratulations! You have passed the quiz with full marks because of your hardwork and dedication towards studies. Keep it up! Check your results here.");
+        }
         try {
             GameHome = FXMLLoader.load(getClass().getResource("/FXML/GameHome.fxml"));
         } catch (IOException e) {
@@ -41,5 +52,7 @@ public class QuizGameResult implements Initializable {
     public void PlayAgain(ActionEvent actionEvent) {
         anchorPane.getChildren().clear();
         anchorPane.getChildren().addAll(GameHome);
+        point =0;
+        QuizGameController.setCorrectInt(0);
     }
 }
